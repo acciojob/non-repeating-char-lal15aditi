759632d95511bb3f352deddc60240e16a94cff35
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-09-20 16:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let counter = {};
+	// counter.str[0] = 1;
+	for(let i = 0; i<str.length; i++){
+		// console.log(counter, i);
+		let key = str[i];
+		if(key in counter){
+			counter[key] = counter[key] + 1;
+		}
+		else{
+			counter[key] = 1;
+		}
+	}
+	for (let i = 0; i < str.length; i++) {
+		let key = str[i];
+		if(counter[key] == 1){
+			return key;
+		}
+	}
+	return null;
+}
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input));
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
